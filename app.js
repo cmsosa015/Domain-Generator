7a4afa3e58d5494b1fe560f00abe8e79cdfcafbc
cmsosa015 <cmsosa015@gmail.com>
2021-06-14 21:54:38 +0000

    second commit

--- app.js
+++ app.js
@@ -4,18 +4,27 @@ const arr1 = ['the', 'le', 'our', 'her']
 const arr2 = ['grand', 'royal', 'gold', 'great']
 const arr3 = ['ballroom', 'bathtub', 'rug', 'globe']
 
-function domain() {
-    for (i = 0; i < arr1.length; i++) {
-        for (j = 0; j < arr2[i].length; j++) {
 
-            for (b = 0; b < arr3[i][j].length; b++)
+function array() {
+    for (let i = 0; i < arr1.length; i++) { 
+
+        for ( let j = 0; j < arr2.length; j++) { 
+
+            for ( let b = 0; b < arr3.length; b++) { 
+                // console.log(arr1[i] + arr2[j] + arr3[b] + ".com");
+                let domain = document.createElement('P')
+                domain.innerText = arr1[i] + arr2[j] + arr3[b] + ".com" 
+                document.querySelector(".domainhere").appendChild(domain)
+
+            }
         }
-    }
+     }
+ }
 
-    let compiled= [arr1+arr2+arr3]
 
-    document.querySelector(".domainhere").innerHTML = compiled ; 
-}
+
+    
+
 
 
 
